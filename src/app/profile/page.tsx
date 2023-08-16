a6d2e613c756dc006291a73f2ac32afce28c873c
aishwary11 <aishwary46@gmail.com>
2023-08-16 22:31:50 +0530

    some change

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -37,7 +37,7 @@ export default function ProfilePage() {
 				Logout
 			</button>
 			<button onClick={getUserDetails} className='bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-				GetUser Details
+				Get User Details
 			</button>
 		</div>
 	);
